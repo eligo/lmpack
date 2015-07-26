89c41ce7354a57d60730bbe520343af0fc74d022
eligo <596840317@qq.com>
2015-07-26 11:05:30 +0800

    aaa

--- a.lua
+++ a.lua
@@ -1,12 +1,12 @@
 local mp = require("lmpack")
 
 local stm = os.clock()
-for i=1, 10 do
+for i=1, 1000000 do
 	local pa = mp.create()
 	local pb = mp.create()
 	pb:pack("hello1", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab")
-	pa:pack(pb, 1,1,1)
-	--local a, b,c,d,e= pa:unpack(2)
+	pa:pack(pb, 1111111,12222222222,1333333333,"wwwwwwwwwwwwwwwwwwd")
+	--local a, b,c,d,e= pa:unpack(5)
 	--print(a,b,c,d,e)
 	--print(a:unpack(2))
 	--break
